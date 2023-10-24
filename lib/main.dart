d0aaefc151f2d0b7512bdfbc80c97834a92f8608
RoseannTejano <126366830+RoseannTejano@users.noreply.github.com>
2023-10-24 09:40:05 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Valley - Admin, Students and Teachers',
       home: const LandingScreen(),
       routes: {
