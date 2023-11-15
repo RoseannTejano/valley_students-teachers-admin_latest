a002de2d73e54a410b94c5338f367d1d38bfc59c
Lans12345 <olanalans12345@gmail.com>
2023-11-15 12:51:45 +0800

    added admin interfaces

--- lib/screens/admin/admin_home.dart
+++ lib/screens/admin/admin_home.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../services/add_user.dart';
 import '../../utils/routes.dart';
 import '../../widgets/button_widget.dart';
+import '../../widgets/text_widget.dart';
+import '../../widgets/textfield_widget.dart';
+import '../../widgets/toast_widget.dart';
 
 class AdminHome extends StatefulWidget {
   const AdminHome({super.key});
@@ -55,9 +61,10 @@ class _AdminHomeState extends State<AdminHome> {
                           radius: 100,
                           height: 60,
                           color: Colors.white,
-                          label: 'Students',
+                          label: 'Register',
                           onPressed: () {
-                            Navigator.pushNamed(context, Routes().adminstudent);
+                            registerDialog();
+                            // Navigator.pushNamed(context, Routes().adminstudent);
                           },
                         ),
                         const SizedBox(
@@ -68,7 +75,25 @@ class _AdminHomeState extends State<AdminHome> {
                           radius: 100,
                           height: 60,
                           color: Colors.white,
-                          label: 'Teachers',
+                          label: 'Laboratory Reservation',
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return reservation();
+                              },
+                            );
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ButtonWidget(
+                          fontColor: Colors.black,
+                          radius: 100,
+                          height: 60,
+                          color: Colors.white,
+                          label: 'Special Announcement',
                           onPressed: () {
                             Navigator.pushNamed(context, Routes().mainhome);
                           },
@@ -80,6 +105,247 @@ class _AdminHomeState extends State<AdminHome> {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  final nameController = TextEditingController();
+
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  registerDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+              backgroundColor: Colors.white.withOpacity(0.8),
+              child: Container(
+                color: Colors.white.withOpacity(0.5),
+                height: 375,
+                width: 300,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFieldWidget(
+                          label: 'Full Name', controller: nameController),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFieldWidget(
+                          label: 'Email', controller: emailController),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFieldWidget(
+                          isPassword: true,
+                          isObscure: true,
+                          label: 'Password',
+                          controller: passwordController),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ButtonWidget(
+                          color: Colors.black,
+                          label: 'Register',
+                          onPressed: (() async {
+                            final email = emailController.text;
+                            final password = passwordController.text;
+                            if (isValidEmail(email) &&
+                                isValidPassword(password)) {
+                              try {
+                                await FirebaseAuth.instance
+                                    .createUserWithEmailAndPassword(
+                                        email: emailController.text,
+                                        password: passwordController.text);
+                                // addUser(
+                                //     newNameController
+                                //         .text,
+                                //     newEmailController
+                                //         .text,
+                                //     newPassController
+                                //         .text);
+                                addUser(
+                                    nameController.text,
+                                    emailController.text,
+                                    passwordController.text,
+                                    'Student');
+                                Navigator.pop(context);
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: TextRegular(
+                                        text: 'Account created succesfully!',
+                                        fontSize: 14,
+                                        color: Colors.white),
+                                  ),
+                                );
+                              } catch (e) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: TextRegular(
+                                        text: e.toString(),
+                                        fontSize: 14,
+                                        color: Colors.white),
+                                  ),
+                                );
+                              }
+                            } else {
+                              showToast(
+                                  'Invalid email and password combination!');
+                            }
+                          })),
+                    ],
+                  ),
+                ),
+              ));
+        });
+  }
+
+  bool isValidEmail(String email) {
+    // Validate email using a regular expression for CSPC email format.
+
+    if (email.contains('@my.cspc.edu.ph')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isValidPassword(String password) {
+    // Check if password has at least 8 characters and contains a combination of upper, lower, and a number.
+    return password.length >= 8 &&
+        password.contains(RegExp(r'[A-Z]')) &&
+        password.contains(RegExp(r'[0-9]'));
+  }
+
+  Widget reservation() {
+    return Dialog(
+      child: SizedBox(
+        width: 500,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 50),
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder<QuerySnapshot>(
+                        stream: FirebaseFirestore.instance
+                            .collection('Reservations')
+                            .where('status', isEqualTo: 'Pending')
+                            .snapshots(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (snapshot.hasError) {
+                            print(snapshot.error);
+                            return const Center(child: Text('Error'));
+                          }
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Padding(
+                              padding: EdgeInsets.only(top: 50),
+                              child: Center(
+                                  child: CircularProgressIndicator(
+                                color: Colors.black,
+                              )),
+                            );
+                          }
+
+                          final data = snapshot.requireData;
+                          return Expanded(
+                            child: SizedBox(
+                              child: ListView.builder(
+                                itemCount: data.docs.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10,
+                                        bottom: 10,
+                                        left: 20,
+                                        right: 20),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        const Icon(
+                                          Icons.calendar_month_outlined,
+                                          size: 48,
+                                        ),
+                                        const SizedBox(
+                                          width: 30,
+                                        ),
+                                        TextBold(
+                                            text: data.docs[index]['name'],
+                                            fontSize: 16,
+                                            color: Colors.black),
+                                        const SizedBox(
+                                          width: 50,
+                                        ),
+                                        TextRegular(
+                                            text: data.docs[index]['date'] +
+                                                ' ' +
+                                                data.docs[index]['time'],
+                                            fontSize: 14,
+                                            color: Colors.black),
+                                        const SizedBox(
+                                          width: 30,
+                                        ),
+                                        IconButton(
+                                          onPressed: () async {
+                                            await FirebaseFirestore.instance
+                                                .collection('Reservations')
+                                                .doc(data.docs[index].id)
+                                                .update({'status': 'Accepted'});
+                                          },
+                                          icon: const Icon(
+                                            Icons.check,
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        IconButton(
+                                          onPressed: () async {
+                                            await FirebaseFirestore.instance
+                                                .collection('Reservations')
+                                                .doc(data.docs[index].id)
+                                                .delete();
+                                          },
+                                          icon: const Icon(
+                                            Icons.delete,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          );
+                        }),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
